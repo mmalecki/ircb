ff7a687f91f34555a3c62cabfc690599cfa9c349
Maciej Małecki <me@mmalecki.com>
2012-10-15 06:35:12 +0200

    [fix test] Fix command parser test

--- test/command-parser-test.js
+++ test/command-parser-test.js
@@ -1,42 +1,42 @@
 var assert = require('assert'),
-    parse = require('../lib/irc2/command-parser');
+    parse = require('../lib/irc2/parser/command-parser');
 
-assert.deepEqual(parse(':lindbohm.freenode.net NOTICE * :*** Found your hostname\r\n'), {
+assert.deepEqual(parse(':lindbohm.freenode.net NOTICE * :*** Found your hostname'), {
   prefix: 'lindbohm.freenode.net',
   command: 'NOTICE',
   middle: ['*'],
   trailing: '*** Found your hostname'
 });
 
-assert.deepEqual(parse('PING :pratchett.freenode.net\r\n'), {
+assert.deepEqual(parse('PING :pratchett.freenode.net'), {
   prefix: null,
   command: 'PING',
   middle: [],
   trailing: 'pratchett.freenode.net'
 });
 
-assert.deepEqual(parse(':barjavel.freenode.net 372 mmalecki__ :- the network!\r\n'), {
+assert.deepEqual(parse(':barjavel.freenode.net 372 mmalecki__ :- the network!'), {
   prefix: 'barjavel.freenode.net',
   command: '372',
   middle: ['mmalecki__'],
   trailing: '- the network!'
 });
 
-assert.deepEqual(parse(':source 372 many middle :- the network!\r\n'), {
+assert.deepEqual(parse(':source 372 many middle :- the network!'), {
   prefix: 'source',
   command: '372',
   middle: ['many', 'middle'],
   trailing: '- the network!'
 });
 
-assert.deepEqual(parse(':source 372 many middle\r\n'), {
+assert.deepEqual(parse(':source 372 many middle'), {
   prefix: 'source',
   command: '372',
   middle: ['many', 'middle'],
   trailing: null
 });
 
-assert.deepEqual(parse(':source 372 many middle :with trailing\r\n'), {
+assert.deepEqual(parse(':source 372 many middle :with trailing'), {
   prefix: 'source',
   command: '372',
   middle: ['many', 'middle'],

b7cef09462564eaca696045acf9c3ec27f76a329
Maciej Małecki <me@mmalecki.com>
2013-01-08 20:10:52 +0100

    [doc] Add a simple example

---
+++ examples/simple.js
@@ -0,0 +1,14 @@
+var ircb = require('../');
+
+var irc = ircb({
+  host: 'irc.freenode.org',
+  secure: true,
+  username: 'mycoolbot',
+  realName: 'mycoolbot',
+  nick: 'mycoolbot',
+  channels: ['#node.js']
+}, function () {
+  irc.on('names', function (channel, names) {
+    console.log(channel + ': ' + names.join(', '));
+  });
+});
